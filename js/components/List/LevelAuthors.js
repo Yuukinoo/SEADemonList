b64ae44db5b4a0ca355173d6a62b883dfc48b046
Prometheus <33342399+Prometheus-03@users.noreply.github.com>
2025-08-03 23:56:55 +0800

    Hah

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -15,26 +15,18 @@ export default {
     },
     template: `
         <div class="level-authors">
-            <template>
-                <div class="type-title-sm">Publisher</div>
-                <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
-                </p>
-                <div class="type-title-sm">Country</div>
-                <p class="type-body">
-                    <span>{{ country }}</span>
-                </p>
-            </template>
+            <div class="type-title-sm">Publisher</div>
+            <p class="type-body">
+                <span>{{ author }}</span>
+            </p>
+            <div class="type-title-sm">Verifier</div>
+            <p class="type-body">
+                <span>{{ verifier }}</span>
+            </p>
+            <div class="type-title-sm">Country</div>
+            <p class="type-body">
+                <span>{{ country }}</span>
+            </p>
         </div>
     `,
-
-    computed: {
-        selfVerified() {
-            return this.author === this.verifier && this.creators.length === 0;
-        },
-    },
 };
